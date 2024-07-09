bb37cc2ae74218e5599c606613862348599dbe99
AtharvaGurao <140331187+AtharvaGurao@users.noreply.github.com>
2024-07-09 12:33:00 +0000

    1st

---
+++ code.java
@@ -0,0 +1,16 @@
+/* Program to print an Integer(Enterted by the user) */
+
+import java.util.Scanner;
+
+
+
+public class code {
+
+    public static void main(String[] args) {
+        System.out.println("Please Enter a number to produce:");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        System.out.println("The number enter by you is :"+number);
+    }
+}
