cf55c5ba767df55df2fffe049a9acbab0f16a906
AtharvaGurao <140331187+AtharvaGurao@users.noreply.github.com>
2024-07-09 12:50:44 +0000

    2

---
+++ code_1.java
@@ -0,0 +1,23 @@
+/*Program to add two numbers */
+
+import java.util.Scanner;
+public class code_1 {
+
+
+    public static void main(String[] args) {
+     System.out.println("Addition of two numbers");
+     System.out.println("Enter First Number:");
+     Scanner scanner = new Scanner(System.in);
+     int Number_1 = scanner.nextInt();
+
+     System.out.println("Enter Second Number:");
+     Scanner scanner2 = new Scanner(System.in);
+     int Number_2 = scanner2.nextInt();
+
+     System.out.println(Number_1 + Number_2);
+
+     
+
+    }
+    
+}
